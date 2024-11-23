3202c1fd54d7a8a0a8bf6e2f673033b5a7c0dc81
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2024-11-22 21:06:01 -0500

    servo 3

--- prototipo_braille_gates/prototipo_braille_gates.ino
+++ prototipo_braille_gates/prototipo_braille_gates.ino
@@ -9,7 +9,7 @@ Servo servoArrived;
 const int buttonPin = 14;
 const int servoArrivedPin = 4; 
 const int servo3minPin = 14; 
-const int servo10minPin = 12;
+const int servo5minPin = 12;
 
 int buttonState = HIGH;  
 int lastButtonState = HIGH;
@@ -18,7 +18,7 @@ const int servoStartPos = 0;
 const int servoActivePos = 90;
 
 void setup() {
-  servo10min.attach(servo10minPin);
+  servo10min.attach(servo5minPin);
   servo3min.attach(servo3minPin);
   servoArrived.attach(servoArrivedPin);
   
@@ -41,20 +41,29 @@ void loop() {
     // randomServo.write(servoActivePos);
 
     Serial.println("Activating servo1");
+    servo5min.write(servoActivePos);
+
+    delay(10000);
+
+    Serial.println("Deactivating servo1");
+    servo5min.write(servoStartPos);
+
+    Serial.println("Activating servo2");
     servo3min.write(servoActivePos);
 
     delay(2000);
 
-    Serial.println("Deactivating servo1");
+    Serial.println("Deactivating servo2");
     servo3min.write(servoStartPos);
 
-    Serial.println("Activating servo2");
+    Serial.println("Activating servo3");
     servoArrived.write(servoActivePos);
 
-    delay(2000);
-
-    Serial.println("Deactivating servo2");
+    delay(5000);
+    
+    Serial.prinln("Deactivating servo3");
     servoArrived.write(servoStartPos);
+
     lastButtonState = HIGH;
   } else {
     lastButtonState = buttonState;
