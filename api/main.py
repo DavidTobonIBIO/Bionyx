559cb7ac54dafe4b7b07b5b24ddd0c58f91af2ee
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2025-02-26 12:29:58 -0500

    simulated api

---
+++ api/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from models import Station, stations, update_bus_locations
+import asyncio
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up and launching background tasks...")
+    task = asyncio.create_task(update_bus_locations())
+    yield
+    task.cancel()
+    print("Shutting down.")
+    
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Bus API"}
+
+@app.get("/stations/")
+async def read_stations(response_model=list[Station]):
+    return list(stations.values())
+
+@app.get("/stations/{name}", response_model=Station)
+async def read_station(name: str):
+    if name not in stations:
+        raise HTTPException(status_code=404, detail=f"Station {name} not found")
+    return stations[name]
+
+    
---
+++ api/models.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+import asyncio
+import random
+
+STATION_NAMES = ["Universidades", "Aguas", "Portal 80", "Portal 170"]
+
+
+class Bus(BaseModel):
+    id: int
+    route: str
+    destination: str
+
+
+class Station(BaseModel):
+    name: str
+    latitude: float = 0.0
+    longitude: float = 0.0
+    arrivingRoutes: list[Bus] = []
+
+
+STATION_NAMES = ["Universidades", "Aguas", "Portal 80", "Portal 170"]
+ROUTES = ["D24", "6", "8", "1", "B75", "B13"]
+DESTINATIONS = ["Portal 80", "Portal 80", "Portal Norte", "Portal El Dorado", "Portal del Norte"]
+
+stations = {name.replace(" ", "").lower(): Station(name=name) for name in STATION_NAMES}
+
+buses = [
+    Bus(id=i, route=route, destination=destination)
+    for i, (route, destination) in enumerate(zip(ROUTES, DESTINATIONS))
+]
+
+
+async def update_bus_locations():
+    while True:
+        for station in stations.values():
+            station.arrivingRoutes = random.sample(buses, k=random.randint(0, 2))
+        await asyncio.sleep(5)  # update every 5 secs
