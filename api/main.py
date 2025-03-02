a49ce24a4f28107294b4a4d4c58d79ea99392a4b
sergiocanar <s.canar@uniandes.edu.co>
2025-03-02 11:39:51 -0500

    API with TM data

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException
-from models import Station, stations, update_bus_locations
+from models import Station, update_bus_locations, stations_dict
 import asyncio
 from contextlib import asynccontextmanager
 import uvicorn
+from sklearn.metrics.pairwise import haversine_distances
 
 
 @asynccontextmanager
@@ -24,14 +25,31 @@ async def read_root():
 
 @app.get("/stations/", response_model=list[Station])
 async def read_stations():
-    return list(stations.values())
+    return list(stations_dict.values())
 
 
 @app.get("/stations/{name}", response_model=Station)
 async def read_station(name: str):
-    if name not in stations:
+    if name not in stations_dict:
         raise HTTPException(status_code=404, detail=f"Station {name} not found")
-    return stations[name]
+    return stations_dict[name]
+
+@app.get("/nearest_station/", response_model=Station)
+async def read_nearest_station(lat: float, lon: float):
+    
+    x1 = [lat, lon]
+    nearest_station = None
+    nearest_distance = 4.5 # 4.5 km is the maximum distance between two stations
+    for station in stations_dict.values():
+        x2 = [station.lat, station.lon]
+        distance = haversine_distances([x1, x2])[0][1]
+        distance_in_km = distance * 6371 # 6371 is the radius of the Earth
+        if distance_in_km < nearest_distance:
+            nearest_station = station
+            nearest_distance = distance
+            return nearest_station
+        else:
+            raise HTTPException(status_code=404, detail=f"No nearest station found")
 
 
 if __name__ == "__main__":
--- api/models.py
+++ api/models.py
@@ -1,9 +1,14 @@
+import os
+import json
+import unicodedata
 from pydantic import BaseModel
 import asyncio
 import random
 
-STATION_NAMES = ["Universidades", "Aguas", "Portal 80", "Portal 170"]
-
+this_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(this_dir)
+data_dir = os.path.join(parent_dir, "data")
+stations_path = os.path.join(data_dir, "tm_stations.geojson")
 
 class Bus(BaseModel):
     id: int
@@ -17,18 +22,34 @@ class Station(BaseModel):
     longitude: float = 0.0
     arrivingRoutes: list[Bus] = []
 
+with open(stations_path, encoding='utf-8') as f:
+    stations = json.load(f)
+    
+stations_dict = {}
+for feature in stations['features']:
+    station_properties = feature['properties']
+    station_name = station_properties['nombre_estacion']
+    latitud = station_properties['latitud_estacion']
+    longitud = station_properties['longitud_estacion']
+    
+    
+    station_name = unicodedata.normalize('NFKD', station_name).encode('ascii', 'ignore').decode('ascii')
+    station_name = station_name.replace(' ', '_').lower()
+    
+    stations_dict[station_name] = Station(name=station_name, latitude=latitud, longitude=longitud)
+    
 
-STATION_NAMES = ["Universidades", "Aguas", "Portal 80", "Portal 170"]
+print(stations_dict.keys())
+
+
 ROUTES = ["D24", "6", "8", "1", "B75", "B13"]
 DESTINATIONS = [
     "Portal 80",
     "Portal 80",
     "Portal Norte",
     "Portal El Dorado",
-    "Portal del Norte",
+    "Portal Norte",
 ]
-
-stations = {name.replace(" ", "_").lower(): Station(name=name) for name in STATION_NAMES}
 
 buses = [
     Bus(id=i, route=route, destination=destination)
@@ -38,6 +59,6 @@ buses = [
 
 async def update_bus_locations():
     while True:
-        for station in stations.values():
+        for station in stations_dict.values():
             station.arrivingRoutes = random.sample(buses, k=random.randint(0, 2))
         await asyncio.sleep(5)  # update every 5 secs
